54998ec48cf98c8139460913ea572979124649a9
Michael McEniry (UAH) <mcenirm@uah.edu>
2013-05-14 16:52:57 -0500

    Implement grid to radial transformation.

--- src/main/java/cydep/Input.java
+++ src/main/java/cydep/Input.java
@@ -4,6 +4,7 @@ import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.util.Formatter;
 import ucar.ma2.Array;
+import ucar.ma2.Index;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 import ucar.nc2.constants.AxisType;
@@ -13,7 +14,7 @@ import ucar.nc2.dataset.NetcdfDataset;
 import ucar.nc2.dt.RadialDatasetSweep;
 import ucar.nc2.ft.FeatureDatasetFactoryManager;
 
-public final class Input {
+final class Input {
 
     private final String pathname;
     private final String dataVariableName;
@@ -23,12 +24,11 @@ public final class Input {
     private final NetcdfDataset netcdfDataset;
     private final CoordinateAxis1D azimuthAxis;
     private final CoordinateAxis1D distanceAxis;
-    private final Variable azimuthVariable;
-    private final Variable distanceVariable;
+    private final int distanceAxisLength;
     private final Variable dataVariable;
     private final Array dataArray;
 
-    public Input(String pathname, String dataVariableName, Formatter errlog) throws IOException {
+    Input(String pathname, String dataVariableName, Formatter errlog) throws IOException {
         this.pathname = pathname;
         this.dataVariableName = dataVariableName;
         this.errlog = errlog;
@@ -37,21 +37,57 @@ public final class Input {
         this.netcdfDataset = NetcdfDataset.wrap(netcdfFile, NetcdfDataset.getEnhanceAll());
         this.azimuthAxis = (CoordinateAxis1D) netcdfDataset.findCoordinateAxis(AxisType.RadialAzimuth);
         this.distanceAxis = (CoordinateAxis1D) netcdfDataset.findCoordinateAxis(AxisType.RadialDistance);
-        this.azimuthVariable = netcdfDataset.findVariable(azimuthAxis.getFullName());
-        this.distanceVariable = netcdfDataset.findVariable(distanceAxis.getFullName());
+        this.distanceAxisLength = distanceAxis.getShape(0);
         this.dataVariable = netcdfDataset.findVariable(dataVariableName);
         this.dataArray = dataVariable.read();
     }
 
     int getDistanceAxisLength() {
-        return distanceAxis.getShape(0);
+        return distanceAxisLength;
     }
 
     double getDistanceAxisGap() {
         return distanceAxis.getCoordValue(1) - distanceAxis.getCoordValue(0);
     }
 
-    double getValue(Point2D ptDst) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    double getValue(Point2D cartesian) {
+        final double distance = getDistanceFromPoint(cartesian);
+        final double azimuth = getAzimuthFromPoint(cartesian);
+        final int distanceIndex = findDistanceElement(distance);
+        final int azimuthIndex = findAzimuthElement(azimuth);
+        return getValue(azimuthIndex, distanceIndex);
+    }
+
+    double getAzimuthFromPoint(Point2D cartesian) {
+        /*
+         * x and y are swapped because atan2 assume counter-clockwise from 
+         * positive x-axis, but compass is clockwise from north
+         */
+        final double azimuth = (360.0 + Math.toDegrees(Math.atan2(cartesian.getX(), cartesian.getY()))) % 360.0;
+        return azimuth;
+    }
+
+    double getDistanceFromPoint(Point2D cartesian) {
+        final double distance = cartesian.distance(0, 0);
+        return distance;
+    }
+
+    int findDistanceElement(double distance) {
+        return distanceAxis.findCoordElement(distance);
+    }
+
+    int findAzimuthElement(double azimuth) {
+        return azimuthAxis.findCoordElement(azimuth);
+    }
+
+    double getValue(int azimuthIndex, int distanceIndex) {
+        final Index index = dataArray.getIndex();
+        try {
+            final double value = dataArray.getDouble(index.set(azimuthIndex, distanceIndex));
+            return value;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            return Double.NaN;
+        }
     }
 }
--- src/test/java/cydep/TestInput.java
+++ src/test/java/cydep/TestInput.java
@@ -19,10 +19,21 @@ public class TestInput {
         int distanceAxisLength = input.getDistanceAxisLength();
         assertEquals("distance axis length", 920, distanceAxisLength);
         input.getValue(new Point2D.Double(0.0, 0.0));
-        for (int i = 0; i < samples.length; i++) {
-            Trial trial = samples[i];
+        for (int i = 0; i < trials.length; i++) {
+            Trial trial = trials[i];
             double value = input.getValue(trial.given);
-            assertEquals("value mismatch i %s given %s,%s", trial.expected, value, 0.000000001);
+            String msg = String.format("value mismatch i %s given %s,%s", i, trial.given.x, trial.given.y);
+            try {
+                assertEquals(msg, trial.expected, value, 0.0001);
+            }
+            catch (AssertionError e) {
+                double azimuth = input.getAzimuthFromPoint(trial.given);
+                double distance = input.getDistanceFromPoint(trial.given);
+                int azimuthIndex = input.findAzimuthElement(azimuth);
+                int distanceIndex = input.findDistanceElement(distance);
+                errlog.format("index [%s][%s] azi %s dist %s\n", azimuthIndex, distanceIndex, azimuth, distance);
+                throw e;
+            }
         }
     }
 
@@ -31,15 +42,26 @@ public class TestInput {
         final Point2D.Double given;
         final double expected;
 
-        public Trial(double expected, double givenX, double givenY) {
-            this.expected = expected;
+        public Trial(double givenX, double givenY, double expected) {
             this.given = new Point2D.Double(givenX, givenY);
+            this.expected = expected;
         }
     }
-    Trial[] samples = {
-        new Trial(Double.NaN, -125.0, 125.0),
-        new Trial(Double.NaN, 125.0, 125.0),
-        new Trial(Double.NaN, -125.0, -125.0),
-        new Trial(Double.NaN, 125.0, -125.0)
+    Trial[] trials = {
+        new Trial(-125.0, 125.0, Double.NaN),
+        new Trial(125.0, 125.0, Double.NaN),
+        new Trial(-125.0, -125.0, Double.NaN),
+        new Trial(125.0, -125.0, Double.NaN),
+        new Trial(143604.67, 119409.20, 0.307),
+        new Trial(44728.02, -133149.69, 0.022),
+        new Trial(-175200.99, -177720.80, Double.NaN),
+        new Trial(-153018.22, -143054.83, 0.173),
+        new Trial(189877.31, 118407.53, 0.064),
+        new Trial(94767.80, -101465.10, 0.116),
+        new Trial(-92945.35, 207099.19, Double.NaN),
+        new Trial(-54490.16, -35230.11, 0.327),
+        new Trial(159015.06, 141263.46, 0.082),
+        new Trial(-224938.87, 91276.47, Double.NaN),
+        new Trial(0.0, 0.0, Double.NaN)
     };
 }
